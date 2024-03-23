759a596ebf52106d02adbe2ab4ac1f54e3b73914
vazlope <pablo.vazquez.lopez@protonmail.com>
2024-03-23 13:21:22 +0100

    la leche calentita

--- portafolio/portafolio.py
+++ portafolio/portafolio.py
@@ -13,7 +13,7 @@ DATA = data.data
 
 def index() -> rx.Component:
     return rx.center(
-        # rx.theme_panel(),
+        rx.theme_panel(),
         rx.vstack(
             header(DATA),
             about(DATA.about),
@@ -38,8 +38,8 @@ app = rx.App(
     stylesheets=STYLESHEETS,
     style=BASE_STYLE,
     theme=rx.theme(
-        appearance="inherit",
-        accent_color="blue",
+        appearance="dark",
+        accent_color="cyan",
         radius="full"
     )
 )
